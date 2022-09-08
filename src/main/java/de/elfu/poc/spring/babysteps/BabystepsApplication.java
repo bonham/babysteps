2fa361bd71099dab0be93d114df4c3d762d3cde5
Art Furrer <2823246+bonham@users.noreply.github.com>
2022-09-08 11:41:37 +0200

    first commit

---
+++ src/main/java/de/elfu/poc/spring/babysteps/BabystepsApplication.java
@@ -0,0 +1,13 @@
+package de.elfu.poc.spring.babysteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabystepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabystepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/elfu/poc/spring/babysteps/BabystepsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.elfu.poc.spring.babysteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabystepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
